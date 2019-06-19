c6cdcfb9ab71c8206efc9845bd7e6d223f94efdc
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 20:08:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,7 @@ def play(songs)
 puts "Please enter a song name or number:"
 input = gets.chomp 
 songs.each do |song|
-  if song.to_i[songs.to_i]
+  if song.to_i.between?
     puts "Playing #{song}"
 end
 end
