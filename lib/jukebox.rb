8e3451c59eb4575ad2f8eaf5ad5de4dc8897da69
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 18:40:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,9 @@
 def help 
+puts "I accept the following commands: /n
+- help : displays this help message /n
+- list : displays a list of songs you can play /n
+- play : lets you choose a song to play /n
+- exit : exits this program"
 end
 
 def list 
